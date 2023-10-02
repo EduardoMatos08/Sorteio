fc991fd59e02e77c0f22ecafed98186aacbcd1f8
EduardoMatos08 <matos08edu@gmail.com>
2023-10-02 16:31:30 -0300

    valor máximo menor que o valor mínimo gera alert

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function randomizerNumber(){
         alert('O valor mínimo tem de ser MENOR que o valor máximo...')
     }
 
+    if(numMax <= numMin) {
+        alert('O valor máximo tem de ser MAIOR que o valor mínimo...')
+    }
+
     else {
         const randomizedNum = Math.floor(Math.random() * (numMax - numMin) + numMin)
         alert(`O número ${randomizedNum} foi sorteado!`)
