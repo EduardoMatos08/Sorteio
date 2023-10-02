8b107a28bdc75b6fe90418c262bd55ea97d34c8f
EduardoMatos08 <matos08edu@gmail.com>
2023-10-02 16:27:41 -0300

    alerta caso o número mínimo seja maior que o máximo

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ function randomizerNumber(){
     const numMax = Math.ceil(document.querySelector("#left-input").value)
     const numMin = Math.floor(document.querySelector("#right-input").value)
 
+    if(numMin >= numMax){
+        alert('O valor mínimo tem de ser MENOR que o valor máximo...')
+    }
+
     const randomizedNum = Math.floor(Math.random() * (numMax - numMin) + numMin);
     
     alert(`O número ${randomizedNum} foi sorteado!`)
