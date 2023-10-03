4524d0f80f45e7b87377de3bb95499e5a161117b
Eduardo Matos <142949790+EduardoMatos08@users.noreply.github.com>
2023-10-03 09:24:37 -0300

    Update script.js - erro de "or", confusão com python, mudança para "&&"

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function randomizerNumber(){
         alert("Os números precisam ser diferentes...")
     }
 
-    if(numMax == "" or numMin == ""){
+    if(numMax == "" && numMin == ""){
         alert("Para sortear é preciso digitar um número...")
     }
 
