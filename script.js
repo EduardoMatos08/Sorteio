5866a969bd0f4488eadda18ee0ceeda71c8bb775
Eduardo Matos <142949790+EduardoMatos08@users.noreply.github.com>
2023-10-03 08:07:53 -0300

    Atualizar o script.js - Alert caso o input esteja vasio

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function randomizerNumber(){
     const numMax = Math.ceil(document.querySelector("#left-input").value)
     const numMin = Math.floor(document.querySelector("#right-input").value)
 
-    if(numMin >= numMax) {
-        alert('O valor mínimo tem de ser MENOR que o valor máximo...')
-    }
-
     if(numMax <= numMin) {
         alert('O valor máximo tem de ser MAIOR que o valor mínimo...')
     }
+
+    if(numMin === "" or numMax === ""){
+        alert("Para sortear você precisa digitar um número...")
+}
 
     else {
         const randomizedNum = Math.floor(Math.random() * (numMax - numMin) + numMin)
