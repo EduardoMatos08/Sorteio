c2bed1db7a0da4c6a52a5e90a97ff95c66a343a7
EduardoMatos08 <matos08edu@gmail.com>
2023-10-03 13:37:24 -0300

    ordem dos ifs e else ifs foi alterada

--- script.js
+++ script.js
@@ -3,16 +3,16 @@ function randomizerNumber(){
     const numMax = Math.ceil(document.querySelector("#right-input").value)
     const numMin = Math.floor(document.querySelector("#left-input").value)
 
-    if(numMax < numMin) {
+    if(numMax == "" || numMin == ""){
+        alert("Para sortear é preciso digitar um número...")
+    }
+
+    else if(numMax < numMin) {
         alert("O valor máximo tem de ser MAIOR que o valor mínimo...")
     }
 
     else if(numMax == numMin){
         alert("Os números precisam ser diferentes...")
-    }
-
-    else if(numMax == "" || numMin == ""){
-        alert("Para sortear é preciso digitar um número...")
     }
 
     else {
