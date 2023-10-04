c6fde8e5fc140af0f3cb54963d12a0797126d899
Eduardo Matos <142949790+EduardoMatos08@users.noreply.github.com>
2023-10-04 09:19:23 -0300

    Update script.js - volta para o commit anterior

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ function randomizerNumber(){
     const numMax = Math.ceil(document.querySelector("#right-input").value)
     const numMin = Math.floor(document.querySelector("#left-input").value)
 
-    if(numMax == numMin){
-        alert("Os números precisam ser diferentes...")
-    }
+    if(numMax == "" || numMin == ""){
+        alert("Para sortear é preciso digitar um número...")
+     }
     
     else if(numMax < numMin) {
         alert("O valor máximo tem de ser MAIOR que o valor mínimo...")
     }
 
-    else if(numMax == "" || numMin == ""){
-        alert("Para sortear é preciso digitar um número...")
-     }
+    else if(numMax == numMin){
+        alert("Os números precisam ser diferentes...")
+    }
 
     else {
         const randomizedNum = Math.floor(Math.random() * (numMax - numMin) + numMin)
